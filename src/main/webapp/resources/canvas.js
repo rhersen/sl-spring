fc882946ea80f619a44f0b5c424f9328b81ccb9b
unknown <rudolf@.(none)>
2011-08-24 22:47:43 +0200

    new coding standard for JavaScript

--- src/main/webapp/resources/canvas.js
+++ src/main/webapp/resources/canvas.js
@@ -66,6 +66,12 @@ function getCurrentTimeMillis(currentDate) {
 }
 
 function getLineHeight(canvas) {
+    var c = canvas.getContext('2d');
+
+    c.font = "100px sans-serif";
+
+    return reduce($(getDepartureArray().map(getLineHeightToFitWidth)), getLineHeightToFitHeight(canvas), max);
+
     function reduce(a, identity, f) {
         if (a.length === 0) {
             return identity;
@@ -74,139 +80,151 @@ function getLineHeight(canvas) {
         }
     }
 
-    var lineHeight = getLineHeightToFitHeight(canvas);
-    var c = canvas.getContext('2d');
+    function getDepartureArray() {
+        return $.makeArray($(state.departures));
+    }
 
-    c.font = "100px sans-serif";
-
-    var a = $($.makeArray($(state.departures)).map(function (departure) {
-        var textLeft = departure.time + ' ' + departure.fullDestination;
+    function getLineHeightToFitWidth(departure) {
         var countdown = getCountdown(departure.time, getCurrentTimeMillis(state.currentDate));
-        var width = c.measureText(textLeft).width + c.measureText(countdown).width;
+        var width = c.measureText(getLeftText(departure)).width + c.measureText(countdown).width;
         return 99 * canvas.width / width;
-    }));
 
-    return reduce(a, lineHeight,
-        function (x, y) {
-            if (x < y) {
-                return x;
-            } else {
-                return y;
-            }
-        });
+        function getLeftText(departure) {
+            return departure.time + ' ' + departure.fullDestination;
+        }
+    }
+
+    function getLineHeightToFitHeight(canvas) {
+        if (state.departures) {
+            return canvas.height / (state.departures.length + 2);
+        } else {
+            return 100;
+        }
+    }
+
+    function max(x, y) {
+        if (x < y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
 }
 
 function draw(canvas) {
+    var c = canvas.getContext('2d');
+    var lineHeight = getLineHeight(canvas);
+
+    clear();
+    setStyle();
+    drawHeader();
+    $(state.departures).each(drawLine);
+
+    function clear() {
+        c.fillStyle = state.responseStatus.getBg();
+        c.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function setStyle() {
+        c.font = lineHeight + "px sans-serif";
+        c.fillStyle = '#fff';
+    }
+
+    function drawHeader() {
+        c.fillText(state.stationName, 4, lineHeight);
+        c.fillText(getStatusString(), 4, lineHeight * 2);
+    }
+
+    function drawLine(i, departure) {
+        var y = lineHeight * (i + 3);
+        drawTextLeft(departure.time + ' ' + departure.fullDestination);
+        drawTextRight(getCountdown(departure.time, getCurrentTimeMillis(state.currentDate)));
+
+        function drawTextLeft(text) {
+            c.fillText(text, 8, y);
+        }
+
+        function drawTextRight(text) {
+            var rightEdge = canvas.width;
+            var measured = c.measureText(text);
+            c.fillText(text, rightEdge - measured.width, y);
+        }
+    }
+
     function getStatusString() {
 //        return getMillisSinceUpdate() + " " + state.millis.getRequest() + " " + state.millis.getResponse() + " " + state.responseStatus.get();
         return touchHandler.n + "|" + touchHandler.x + "|" + touchHandler.y;
     }
-
-    var c = canvas.getContext('2d');
-    var lineHeight = getLineHeight(canvas, c);
-    c.font = lineHeight + "px sans-serif";
-
-    c.fillStyle = state.responseStatus.getBg();
-    c.fillRect(0, 0, canvas.width, canvas.height);
-
-    c.fillStyle = '#fff';
-    c.fillText(state.stationName, 4, lineHeight);
-    c.fillText(getStatusString(), 4, lineHeight * 2);
-
-    $(state.departures).each(function (i, departure) {
-        function getY(i) {
-            return lineHeight * (i + 3);
-        }
-
-        function fillTextRight(countdown) {
-            var rightEdge = canvas.width;
-            var measured = c.measureText(countdown);
-            c.fillText(countdown, rightEdge - measured.width, getY(i));
-        }
-
-        var textLeft = departure.time + ' ' + departure.fullDestination;
-        var countdown = getCountdown(departure.time, getCurrentTimeMillis(state.currentDate));
-        c.fillText(textLeft, 8, getY(i));
-        fillTextRight(countdown);
-    });
-}
-
-function getLineHeightToFitHeight(canvas) {
-    if (state.departures) {
-        return canvas.height / (state.departures.length + 2);
-    } else {
-        return 100;
-    }
 }
 
 function init(id, direction) {
-    function setStation(id) {
-        function getAjaxUrl() {
-            return "departures?id=" + state.stationId + "&direction=" + 'ns';
+    var canvas = getCanvas();
+    state.stationId = id;
+    setInterval(run, 256);
+    window.onresize = handleResize;
+    canvas.ontouchstart = function (event) { return touchHandler.handleTouch(event)};
+    handleResize();
+
+    function run() {
+        state.currentDate = new Date();
+
+        draw(canvas);
+
+        if (shouldUpdate()) {
+            setStation(state.stationId);
         }
 
-        function handleSuccess(data, status) {
-            state.millis.responseReceived();
+        function shouldUpdate() {
+            return isOutdated(getMillisSinceUpdate(), state.millis.getRequest(), state.millis.getResponse());
+        }
 
-            if (!data) {
-                state.responseStatus.set("no data");
-                return;
+        function setStation(id) {
+            state.stationId = id;
+            state.millis.requestSent();
+            state.responseStatus.set("");
+
+            $.ajax({url: getAjaxUrl(), success: handleSuccess, error: handleError});
+
+            function getAjaxUrl() {
+                return "departures?id=" + state.stationId + "&direction=" + 'ns';
             }
 
-            state.stationName = data.stationName;
-            state.updated = data.updated;
-            state.departures = data.departures;
+            function handleSuccess(data, status) {
+                state.millis.responseReceived();
 
-            if (getMillisSinceUpdate() > 200000) {
-                state.responseStatus.set("expired");
-            } else {
+                if (!data) {
+                    state.responseStatus.set("no data");
+                    return;
+                }
+
+                state.stationName = data.stationName;
+                state.updated = data.updated;
+                state.departures = data.departures;
+
+                if (getMillisSinceUpdate() > 200000) {
+                    state.responseStatus.set("expired");
+                } else {
+                    state.responseStatus.set(status);
+                }
+
+                handleResize();
+            }
+
+            //noinspection JSUnusedLocalSymbols
+            function handleError(jqXHR, status) {
+                state.millis.responseReceived();
                 state.responseStatus.set(status);
             }
-
-            handleResize();
         }
-
-        //noinspection JSUnusedLocalSymbols
-        function handleError(jqXHR, status) {
-            state.millis.responseReceived();
-            state.responseStatus.set(status);
-        }
-
-        state.stationId = id;
-        state.millis.requestSent();
-        state.responseStatus.set("");
-
-        $.ajax({url: getAjaxUrl(), success: handleSuccess, error: handleError});
     }
 
     function handleResize() {
         var margin = 5;
         state.innerHeight = window.innerHeight;
-        getCanvas().style.marginLeft = margin + "px";
-        getCanvas().style.marginTop = margin + "px";
+        canvas.style.marginLeft = margin + "px";
+        canvas.style.marginTop = margin + "px";
 
-        getCanvas().width = window.innerWidth - (margin * 2);
-        getCanvas().height = window.innerHeight - (margin * 2);
+        canvas.width = window.innerWidth - (margin * 2);
+        canvas.height = window.innerHeight - (margin * 2);
     }
-
-    function run() {
-        function shouldUpdate() {
-            return isOutdated(getMillisSinceUpdate(), state.millis.getRequest(), state.millis.getResponse());
-        }
-
-        state.currentDate = new Date();
-
-        draw(getCanvas());
-
-        if (shouldUpdate()) {
-            setStation(state.stationId);
-        }
-    }
-
-    state.stationId = id;
-    setInterval(run, 256);
-    window.onresize = handleResize;
-    var canvas = getCanvas();
-    canvas.ontouchstart = function (event) { return touchHandler.handleTouch(event)};
-    handleResize();
 }
