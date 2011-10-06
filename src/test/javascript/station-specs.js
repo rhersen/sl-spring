f94f290f932185ccf37b41d412acebe9209b46b8
unknown <rudolf@.(none)>
2011-10-06 17:04:45 +0200

    ported station test to jasmine

--- src/test/javascript/station-specs.js
+++ src/test/javascript/station-specs.js
@@ -1,13 +1,128 @@
 describe('station', function() {
 
-    function equals(actual, expected) {
-        expect(actual).toBe(expected);
-    }
+    it("set status", function() {
+        setFixtures('<div id="bg"></div>');
 
-    function ok(actual) {
-        expect(actual).toBeTruthy();
-    }
+        var bg = $("#bg");
+        expect(bg.length).toEqual(1);
+        expect(!bg.hasClass("pending")).toBeTruthy();
+        expect(!bg.hasClass("error")).toBeTruthy();
+        var target = createStatus();
 
-    var test = it;
-    
+        target.set("", bg);
+        expect(bg.hasClass("pending")).toBeTruthy();
+        expect(!bg.hasClass("error")).toBeTruthy();
+        expect(!bg.hasClass("expired")).toBeTruthy();
+        target.set("success", bg);
+        expect(!bg.hasClass("pending")).toBeTruthy();
+        expect(!bg.hasClass("error")).toBeTruthy();
+        expect(!bg.hasClass("expired")).toBeTruthy();
+        target.set("error", bg);
+        expect(!bg.hasClass("pending")).toBeTruthy();
+        expect(bg.hasClass("error")).toBeTruthy();
+        expect(!bg.hasClass("expired")).toBeTruthy();
+        target.set("expired", bg);
+        expect(!bg.hasClass("pending")).toBeTruthy();
+        expect(!bg.hasClass("error")).toBeTruthy();
+        expect(bg.hasClass("expired")).toBeTruthy();
+        target.set("no data", bg);
+        expect(!bg.hasClass("pending")).toBeTruthy();
+        expect(bg.hasClass("error")).toBeTruthy();
+        expect(!bg.hasClass("expired")).toBeTruthy();
+    });
+
+    it("getDirection", function() {
+        setFixtures('<label for="northbound">N</label><input type="checkbox" id="northbound" value="val" /> <label for="southbound">S</label><input type="checkbox" id="southbound" value="val" />');
+
+        expect(getDirection()).toEqual("ns");
+        $("#northbound").prop("checked", true);
+        expect(isNorthChecked()).toBeTruthy();
+        expect(!isSouthChecked()).toBeTruthy();
+        expect(getDirection()).toEqual("n");
+        expect(isNorthChecked()).toBeTruthy();
+        $("#southbound").prop("checked", true);
+        expect(isSouthChecked()).toBeTruthy();
+        expect(getDirection()).toEqual("ns");
+        $("#northbound").prop("checked", false);
+        expect(!isNorthChecked()).toBeTruthy();
+        expect(isSouthChecked()).toBeTruthy();
+        expect(getDirection()).toEqual("s");
+    });
+
+    it("setDirection", function() {
+        setFixtures('<label for="northbound">N</label><input type="checkbox" id="northbound" value="val" /> <label for="southbound">S</label><input type="checkbox" id="southbound" value="val" />');
+
+        setDirection('n');
+        expect(isNorthChecked()).toBeTruthy();
+        expect(!isSouthChecked()).toBeTruthy();
+        setDirection('s');
+        expect(!isNorthChecked()).toBeTruthy();
+        expect(isSouthChecked()).toBeTruthy();
+        setDirection('ns');
+        expect(isNorthChecked()).toBeTruthy();
+        expect(isSouthChecked()).toBeTruthy();
+    });
+
+    it("updateCountdown", function() {
+        setFixtures('<table id="departures"> <tr> <td>8:07</td> <td>station1</td> <td class="countdown">countdown1</td> </tr> <tr> <td>8:11</td> <td>station2</td> <td class="countdown">countdown2</td> </tr> </table> ');
+
+        var rows = $('#departures tr');
+        expect(rows.size()).toEqual(2);
+        var now = new Date(1300000000000);
+        var countdown1 = $('#departures tr:first td:last');
+        var countdown2 = $('#departures tr:last td:last');
+        expect(countdown1.html()).toEqual("countdown1");
+        updateCountdown(now);
+        expect(countdown1.html()).toEqual("0:20.0");
+        expect(countdown2.html()).toEqual("4:20.0");
+    });
+
+    it("updatePage", function() {
+        setFixtures('<table id="departures"> <tr> <td>8:07</td> <td>station1</td> <td class="countdown">countdown1</td> </tr> <tr> <td>8:11</td> <td>station2</td> <td class="countdown">countdown2</td> </tr> </table> ');
+
+        var rows;
+        rows = $('#departures tr');
+        expect(rows.size()).toEqual(2);
+        updatePage({"updated":"22:10","stationName":"Tullinge","departures":[
+                    {"time":"22:26","destination":"Märsta","delayed":false,"direction":"n","millis":1560000},
+                    {"time":"22:56","destination":"Märsta","delayed":false,"direction":"n","millis":3360000},
+                    {"time":"23:28;","destination":"Märsta","delayed":true,"direction":"n","millis":1560000}
+                ]});
+        rows = $('#departures tr');
+        expect(rows.size()).toEqual(3);
+        expect($('#departures tr[class=punctual]').size()).toEqual(2);
+        expect($('#departures tr[class=delayed]').size()).toEqual(1);
+        expect($('#departures tr:first td:first').html()).toEqual("22:26");
+        expect($('#departures tr:first td:eq(1)').html()).toEqual("Märsta");
+        expect($('#departures tr:first td:last').html()).toEqual("countdown");
+    });
+
+    it("filterDirection", function() {
+        setFixtures('<label for="northbound">N</label><input type="checkbox" id="northbound" value="val" /> <label for="southbound">S</label><input type="checkbox" id="southbound" value="val" /><table id="departures"> <tr> <td>8:07</td> <td>station1</td> <td class="countdown">countdown1</td> </tr> <tr> <td>8:11</td> <td>station2</td> <td class="countdown">countdown2</td> </tr> </table> ');
+
+        var rows;
+        rows = $('#departures tr');
+        expect(rows.size()).toEqual(2);
+        var data = {"departures":[
+            {"time":"23:56","destination":"Märsta","delayed":false,"direction":"n"},
+            {"time":"00:26","destination":"Märsta","delayed":false,"direction":"n"},
+            {"time":"00:56","destination":"Märsta","delayed":false,"direction":"n"},
+            {"time":"00:07","destination":"Södertälje hamn","delayed":true,"direction":"s"},
+            {"time":"00:33","destination":"Södertälje hamn","delayed":false,"direction":"s"}
+        ],"updated":"23:56","stationName":"Tullinge"};
+        updatePage(data);
+        rows = $('#departures tr');
+        expect(rows.size()).toEqual(5);
+        $("#northbound").prop("checked", true);
+        $("#southbound").prop("checked", false);
+        updatePage(data);
+        rows = $('#departures tr');
+        expect(rows.size()).toEqual(3);
+        $("#northbound").prop("checked", false);
+        $("#southbound").prop("checked", true);
+        updatePage(data);
+        rows = $('#departures tr');
+        expect(rows.size()).toEqual(2);
+    });
+
 });
--- src/test/javascript/timecalc-specs.js
+++ src/test/javascript/timecalc-specs.js
@@ -1,15 +1,5 @@
 describe('timecalc', function() {
 
-    function equals(actual, expected) {
-        expect(actual).toBe(expected);
-    }
-
-    function ok(actual) {
-        expect(actual).toBeTruthy();
-    }
-
-    var test = it;
-
     function createTimeMock(time, timezoneOffset) {
         var mock = new Object;
         mock.getTime = function () {
@@ -22,81 +12,81 @@ describe('timecalc', function() {
     }
 
     it("should count from midnight", function() {
-        expect(getCountdown("00:01", 0)).toBe("1:00.0");
-        expect(getCountdown("0:02", 0)).toBe("2:00.0");
-        expect(getCountdown("00:20", 0)).toBe("20:00.0");
-        expect(getCountdown("0:59", 0)).toBe("59:00.0");
-        expect(getCountdown("00:50", 0)).toBe("50:00.0");
-        expect(getCountdown("01:00", 0)).toBe("60:00.0");
-        expect(getCountdown("01:10", 0)).toBe("70:00.0");
-        expect(getCountdown("01:39", 0)).toBe("99:00.0");
+        expect(getCountdown("00:01", 0)).toEqual("1:00.0");
+        expect(getCountdown("0:02", 0)).toEqual("2:00.0");
+        expect(getCountdown("00:20", 0)).toEqual("20:00.0");
+        expect(getCountdown("0:59", 0)).toEqual("59:00.0");
+        expect(getCountdown("00:50", 0)).toEqual("50:00.0");
+        expect(getCountdown("01:00", 0)).toEqual("60:00.0");
+        expect(getCountdown("01:10", 0)).toEqual("70:00.0");
+        expect(getCountdown("01:39", 0)).toEqual("99:00.0");
     });
 
     it("30 seconds after midnight", function() {
-        expect(getCountdown("00:01", 30000)).toBe("0:30.0");
-        expect(getCountdown("00:01", 30500)).toBe("0:29.5");
+        expect(getCountdown("00:01", 30000)).toEqual("0:30.0");
+        expect(getCountdown("00:01", 30500)).toEqual("0:29.5");
     });
 
     it("five minutes after midnight", function() {
-        expect(getCountdown("00:05", 300000)).toBe("0:00.0");
-        expect(getCountdown("00:10", 300000)).toBe("5:00.0");
-        expect(getCountdown("00:04", 300000)).toBe("-1:00.0");
+        expect(getCountdown("00:05", 300000)).toEqual("0:00.0");
+        expect(getCountdown("00:10", 300000)).toEqual("5:00.0");
+        expect(getCountdown("00:04", 300000)).toEqual("-1:00.0");
     });
 
     it("ten to one", function() {
-        expect(getCountdown("00:50", 3000000)).toBe("0:00.0");
-        expect(getCountdown("00:55", 3000000)).toBe("5:00.0");
-        expect(getCountdown("00:59", 3000000)).toBe("9:00.0");
-        expect(getCountdown("01:00", 3000000)).toBe("10:00.0");
-        expect(getCountdown("01:10", 3000000)).toBe("20:00.0");
+        expect(getCountdown("00:50", 3000000)).toEqual("0:00.0");
+        expect(getCountdown("00:55", 3000000)).toEqual("5:00.0");
+        expect(getCountdown("00:59", 3000000)).toEqual("9:00.0");
+        expect(getCountdown("01:00", 3000000)).toEqual("10:00.0");
+        expect(getCountdown("01:10", 3000000)).toEqual("20:00.0");
     });
 
     it("one", function() {
-        expect(getCountdown("00:59", 3600000)).toBe("-1:00.0");
-        expect(getCountdown("01:00", 3600000)).toBe("0:00.0");
-        expect(getCountdown("01:10", 3600000)).toBe("10:00.0");
-        expect(getCountdown("02:10", 3600000)).toBe("70:00.0");
+        expect(getCountdown("00:59", 3600000)).toEqual("-1:00.0");
+        expect(getCountdown("01:00", 3600000)).toEqual("0:00.0");
+        expect(getCountdown("01:10", 3600000)).toEqual("10:00.0");
+        expect(getCountdown("02:10", 3600000)).toEqual("70:00.0");
     });
 
     it("two and a half minutes after midnight", function() {
-        expect(getCountdown("00:01", 200000)).toBe("-2:20.0");
-        expect(getCountdown("00:02", 200000)).toBe("-1:20.0");
-        expect(getCountdown("00:03", 200000)).toBe("-0:20.0");
+        expect(getCountdown("00:01", 200000)).toEqual("-2:20.0");
+        expect(getCountdown("00:02", 200000)).toEqual("-1:20.0");
+        expect(getCountdown("00:03", 200000)).toEqual("-0:20.0");
     });
 
     it("regression", function() {
-        expect(getCountdown("00:01", 82000000)).toBe("74:20.0");
+        expect(getCountdown("00:01", 82000000)).toEqual("74:20.0");
     });
 
     it("ten", function() {
-        expect(getCountdown("10:00", 36000000)).toBe("0:00.0");
-        expect(getCountdown("11:00", 36000000)).toBe("60:00.0");
+        expect(getCountdown("10:00", 36000000)).toEqual("0:00.0");
+        expect(getCountdown("11:00", 36000000)).toEqual("60:00.0");
     });
 
     it("getCountdown with invalid time", function() {
-        expect(getCountdown("", 300000)).toBe("?");
+        expect(getCountdown("", 300000)).toEqual("?");
     });
 
     it("getMillisFromMidnight", function() {
-        expect(getMillisFromMidnight("00:01")).toBe(60000);
-        expect(getMillisFromMidnight("00:02")).toBe(120000);
-        expect(getMillisFromMidnight("00:09")).toBe(9 * 60000);
-        expect(getMillisFromMidnight("00:10")).toBe(600000);
-        expect(getMillisFromMidnight("00:59")).toBe(3540000);
-        expect(getMillisFromMidnight("01:00")).toBe(3600000);
+        expect(getMillisFromMidnight("00:01")).toEqual(60000);
+        expect(getMillisFromMidnight("00:02")).toEqual(120000);
+        expect(getMillisFromMidnight("00:09")).toEqual(9 * 60000);
+        expect(getMillisFromMidnight("00:10")).toEqual(600000);
+        expect(getMillisFromMidnight("00:59")).toEqual(3540000);
+        expect(getMillisFromMidnight("01:00")).toEqual(3600000);
     });
 
     it("getMillisSinceRefresh", function() {
-        expect(getMillisSinceRefresh(createTimeMock(0, 0), 0)).toBe(0);
-        expect(getMillisSinceRefresh(createTimeMock(60000, 0), 60000)).toBe(0);
-        expect(getMillisSinceRefresh(createTimeMock(60000, 0), 0)).toBe(60000);
-        expect(getMillisSinceRefresh(createTimeMock(3600000, 60), 0)).toBe(0);
-        expect(getMillisSinceRefresh(createTimeMock(3660000, 60), 0)).toBe(60000);
-        expect(getMillisSinceRefresh(createTimeMock(1310854593304, -120), 240000)).toBe(753304);
+        expect(getMillisSinceRefresh(createTimeMock(0, 0), 0)).toEqual(0);
+        expect(getMillisSinceRefresh(createTimeMock(60000, 0), 60000)).toEqual(0);
+        expect(getMillisSinceRefresh(createTimeMock(60000, 0), 0)).toEqual(60000);
+        expect(getMillisSinceRefresh(createTimeMock(3600000, 60), 0)).toEqual(0);
+        expect(getMillisSinceRefresh(createTimeMock(3660000, 60), 0)).toEqual(60000);
+        expect(getMillisSinceRefresh(createTimeMock(1310854593304, -120), 240000)).toEqual(753304);
         expect(getMillisSinceRefresh(createTimeMock(2 * 60 * 60 * 1000, 0),
-            60 * 60 * 1000)).toBe(60 * 60 * 1000);
+            60 * 60 * 1000)).toEqual(60 * 60 * 1000);
         expect(getMillisSinceRefresh(createTimeMock(60 * 60 * 1000, 0),
-            23 * 60 * 60 * 1000)).toBe(2 * 60 * 60 * 1000);
+            23 * 60 * 60 * 1000)).toEqual(2 * 60 * 60 * 1000);
     });
 
     it("isOutdated", function() {
@@ -118,7 +108,7 @@ describe('timecalc', function() {
         }
         end = new Date().getTime();
 
-        expect(target.getRequest() > 0).toBeTruthy();
+        expect(target.getRequest()).toBeGreaterThan(0);
         expect(target.getRequest() >= target.getResponse()).toBeTruthy();
         expect(target.getResponse() <= end).toBeTruthy();
     });
